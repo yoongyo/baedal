5b6f4319227794a18de8a569f85a6c9418c6930a
yoongyo <jyg0172@naver.com>
2020-04-15 12:08:40 +0900

    DA

--- restaurant/models.py
+++ restaurant/models.py
@@ -43,6 +43,8 @@ class Restaurant(models.Model):
 
     deliverable_area = models.ManyToManyField(Area, related_name='deliverable_area', blank=True)
 
+    delivery_tip = models.CharField(max_length=50)
+
     def __str__(self):
         return self.name
 

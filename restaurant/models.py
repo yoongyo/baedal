5f269061f830dca90177c3b51bcf9e391fed3d5a
yoongyo <jyg0172@naver.com>
2020-04-15 10:59:23 +0900

    AD

---
+++ restaurant/models.py
@@ -0,0 +1,91 @@
+from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class Area(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class RestaurantCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Restaurant(models.Model):
+    admin = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    category = models.ForeignKey(on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    logo = models.ImageField(upload_to='logo')
+    address = models.CharField(max_length=50)
+    tel = models.CharField(max_length=50)
+    introduction = models.TextField()
+
+    closedDay = models.CharField(max_length=50)
+    businessHours = models.CharField(max_length=50)
+
+    businessLicenseRepresentative = models.CharField(max_length=50)
+    businessLicenseMutualName = models.CharField(max_length=50)
+    businessLicenseNumber = models.CharField(max_length=50)
+
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='likes', blank=True)
+    follow = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='follow', blank=True)
+
+    representative_menu = models.CharField(max_length=50)
+    delivery_requirement_time = models.CharField(max_length=50)
+
+    minimum_order_price = models.CharField(max_length=50)
+
+    deliverable_area = models.ManyToManyField(Area, related_name='deliverable_area', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MenuCategory(models.Model):
+    admin = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=50)
+    content = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Menu(models.Model):
+    admin = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    price = models.CharField(max_length=50)
+    image = models.ImageField(null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+    popular = models.BooleanField(default=False)
+    soldOut = models.BooleanField(default=False, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    content = models.TextField()
+    image1 = models.ImageField(upload_to='review_image/', blank=True, null=True)
+    image2 = models.ImageField(upload_to='review_image/', blank=True, null=True)
+    image3 = models.ImageField(upload_to='review_image/', blank=True, null=True)
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.restaurant.name
+
+
+
+
+
---
+++ restaurant/schema.py
@@ -0,0 +1,169 @@
+import graphene
+from graphene_django.types import DjangoObjectType
+from .models import RestaurantCategory, Restaurant, MenuCategory, Menu, Area, Review
+from django.contrib.auth.models import User
+
+
+class AreaType(DjangoObjectType):
+    class Meta:
+        model = Area
+
+
+class AreaInput(graphene.InputObjectType):
+    name = graphene.String()
+
+
+class RestaurantCategoryType(DjangoObjectType):
+    class Meta:
+        model = RestaurantCategory
+
+
+class RestaurantType(DjangoObjectType):
+    class Meta:
+        model = Restaurant
+
+
+class MenuCategoryType(DjangoObjectType):
+    class Meta:
+        model = MenuCategory
+
+
+class MenuType(DjangoObjectType):
+    class Meta:
+        model = Menu
+
+
+class ReviewType(DjangoObjectType):
+    class Meta:
+        model = Review
+
+
+class Query(graphene.AbstractType):
+    all_restaurantCategory = graphene.List(RestaurantCategoryType)
+    restaurant = graphene.Field(RestaurantType,
+                                id=graphene.Int(),
+                                name=graphene.String())
+    menu = graphene.Field(MenuType,
+                          id=graphene.Int(),
+                          restaurant=graphene.Int(),
+                          category=graphene.Int(),
+                          )
+    all_restaurant = graphene.List(RestaurantType,
+                                   name=graphene.String(),
+                                   areaId=graphene.Int(),
+                                   )
+    all_menuCategory = graphene.List(MenuCategoryType,
+                                     restaurant=graphene.Int())
+    all_menu = graphene.List(MenuType,
+                             name=graphene.String(),
+                             id=graphene.Int(),
+                             restaurant=graphene.Int(),
+                             category=graphene.Int())
+    all_area = graphene.List(AreaType)
+    all_review = graphene.List(ReviewType,
+                               restaurant=graphene.Int())
+
+    def resolve_all_area(self, context, **kwargs):
+        return Area.objects.all()
+
+    def resolve_all_restaurantCategory(self, context, **kwargs):
+        return RestaurantCategory.objects.all()
+
+    def resolve_all_restaurant(self, context, **kwargs):
+        name = kwargs.get('name')
+        areaId = kwargs.get('areaId')
+        if name is not None:
+            return Restaurant.objects.filter(name=name)
+
+        if areaId is not None:
+            return Restaurant.objects.filter(area__id=areaId)
+
+        return Restaurant.objects.all()
+
+    def resolve_all_menuCategory(self, context, **kwargs):
+        id = kwargs.get('restaurant')
+        if id is not None:
+            return MenuCategory.objects.filter(restaurant__id=id)
+        return MenuCategory.objects.all()
+
+    def resolve_all_menu(self, context, **kwargs):
+        restaurant = kwargs.get('restaurant')
+        category = kwargs.get('category')
+        name = kwargs.get('name')
+        if name is not None:
+            return Menu.objects.filter(name=name)
+        if restaurant is not None:
+            return Menu.objects.filter(restaurant__id=restaurant)
+        if category is not None:
+            return Menu.objects.filter(category__id=category)
+
+        return Menu.objects.all()
+
+    def resolve_all_review(self, context, **kwargs):
+        id = kwargs.get('restaurant')
+        if id is not None:
+            return Review.objects.filter(restaurant__id=id)
+        return Review.objects.all()
+
+    def resolve_restaurant(self, info, **kwargs):
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+
+        if id is not None:
+            return Restaurant.objects.get(pk=id)
+
+        if name is not None:
+            return Restaurant.objects.get(name=name)
+        return None
+
+    def resolve_menu(self, info, **kwargs):
+        id = kwargs.get('id')
+        restaurant = kwargs.get('restaurant')
+        category = kwargs.get('category')
+
+        if id is not None:
+            return Menu.objects.get(pk=id)
+        if restaurant is not None:
+            return Menu.objects.get(restaurant__id=restaurant)
+        if category is not None:
+            return Menu.objects.get(category__id=category)
+
+        return None
+
+
+class Liked(graphene.Mutation):
+    class Arguments:
+        username = graphene.String()
+        restaurantId = graphene.Int()
+
+    liked = graphene.Field(RestaurantType)
+
+    def mutate(self, info, username, restaurantId):
+        user = User.objects.get(username=username)
+        restaurant = Restaurant.objects.get(pk=restaurantId)
+
+        if restaurant.likes.filter(id=user.id).exists():
+            _liked = restaurant.likes.remove(user)
+        else:
+            _liked = restaurant.likes.add(user)
+
+        return Liked(liked=_liked)
+
+
+class Followed(graphene.Mutation):
+    class Arguments:
+        username = graphene.String()
+        restaurantId = graphene.Int()
+
+    followed = graphene.Field(RestaurantType)
+
+    def mutate(self, info, username, restaurantId):
+        user = User.objects.get(username=username)
+        restaurant = Restaurant.objects.get(pk=restaurantId)
+
+        if restaurant.follow.filter(id=user.id).exists():
+            _followed = restaurant.follow.remove(user)
+        else:
+            _followed = restaurant.follow.add(user)
+
+        return Followed(followed=_followed)
